11bdedf29bb394e585071fdf0f06f8e2e102c584
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-31 18:50:59 -0500

    added a firebase config to travel.js

--- assets/javascript/travel.js
+++ assets/javascript/travel.js
@@ -1,11 +1,17 @@
+// ===== FIREBASE CONFIGURATION ===== 
+var firebaseConfig = {
+    apiKey: "AIzaSyAbOz7Rc8Sy34ZeQnRnZrLrEB0UFisJOuo",
+    authDomain: "project-1-62259.firebaseapp.com",
+    databaseURL: "https://project-1-62259.firebaseio.com",
+    projectId: "project-1-62259",
+    storageBucket: "",
+    messagingSenderId: "811913379053",
+    appId: "1:811913379053:web:3f6e668187ab670e"
+};
+// ===== Initialize Firebase =====
+firebase.initializeApp(firebaseConfig);
+
+// ===== Variable for database reference =====
+var database = firebase.database();
 
 
-
-
-
-
-
-
-
-
-    
